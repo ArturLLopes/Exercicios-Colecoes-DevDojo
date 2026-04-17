ec4c15e7af2eba38fe430c232b25e6138adad79f
ArturLLopes <arturllopes@gmail.com>
2026-04-17 18:09:08 -0300

    LIST: addAll e remove

--- colecoes/src/List/ListEx.java
+++ colecoes/src/List/ListEx.java
@@ -10,13 +10,26 @@ public class ListEx {
         nomes.add("Artur");
         nomes.add("Diego");
 //        nomes.add(1000); sem List<> é add tudo
+        List<String> nomes2 = new ArrayList<>();
+
+        nomes.add("Rayane");
+        nomes.add("Jessica");
+
+        nomes.addAll(nomes2);
+
         for (String nome:nomes){
             System.out.println(nome);
         }
 
-        for (int i = 0; i < nomes.size(); i++) {
+        int size = nomes.size();// fixando o valor do tamanho do array
+        for (int i = 0; i < size; i++) {
             System.out.println(nomes.get(i));
         }
+
+        System.out.println(nomes);
+
+        nomes.remove("Diego");
+        System.out.println(nomes);
 
     }
 }

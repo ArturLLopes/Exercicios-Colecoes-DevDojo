ff828d9cedfebbf8706dc88b099e82fd34bc64d1
ArturLLopes <arturllopes@gmail.com>
2026-04-17 17:33:30 -0300

    Equals: sobrescrevendo o metodo com as regras de negocios

--- colecoes/src/entity/Pessoa.java
+++ colecoes/src/entity/Pessoa.java
@@ -10,6 +10,18 @@ public class Pessoa {
         this.nomeFull = nomeFull;
     }
 
+    //o metodo equals deve ser REFLEXIVO(x = T se nao for null), SIMETRICO, TRANSITIVIDADE(xyz sao = a xyz),CONSISTENTE(X sempre é X)
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null)return  false;
+        if (this == obj) return true;
+        if (getClass() != obj.getClass()) return false;
+
+        Pessoa pessoa = (Pessoa) obj;
+        return nome != null && nome.equals(pessoa.nome);
+
+    }
+
     public String getNome() {
         return nome;
     }

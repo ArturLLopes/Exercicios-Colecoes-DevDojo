4d528ef577f6c9c60ea3eb25ac8556294a107726
ArturLLopes <arturllopes@gmail.com>
2026-04-27 15:25:14 -0300

    WILDCARD: Usado em generics para representar um tipo desconhecido

---
+++ colecoes/src/Generics/WildCardEx.java
@@ -0,0 +1,40 @@
+package Generics;
+
+
+abstract class Animal{
+    public abstract void consulta();
+}
+class  Cachorro extends Animal{
+
+    @Override
+    public void consulta() {
+        System.out.println("Consuntando doguim");
+    }
+}
+class Gato extends Animal{
+
+    @Override
+    public void consulta() {
+        System.out.println("Consuntando o gatim");
+    }
+}
+
+public class WildCardEx {
+    public static void main(String[] args) {
+        Cachorro[] cachorros = {new Cachorro(), new Cachorro()};
+        Gato[] gatos = {new Gato(), new Gato()};
+        printConsulta(cachorros);
+        printConsulta(gatos);
+        Animal[] animals = {new Gato(), new Cachorro()};
+        Animal animal = new Gato();
+        printConsulta(animals);
+
+    }
+
+
+    private static void printConsulta(Animal[] animals){
+        for (Animal animal : animals){
+            animal.consulta();
+        }
+    }
+}
